d0b20f5c7d55e3c156dd5c5861d3006dee39848e
amiralikatebi <amirali.katebi5@gmail.com>
2025-10-06 21:45:52 +0330

    new comit

--- app/news/page.js
+++ app/news/page.js
@@ -1,13 +1,23 @@
-import NewsList from "@/components/NewsList";
+import NewsList from "@/components/NewsList"; // مطمئن شو مسیر درست است
 
+export const dynamic = "force-dynamic";
 export const revalidate = 3600;
 
+const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://amiralikatebi.ir"
+    : "http://localhost:3000";
+
 async function fetchNews() {
-  const res = await fetch(process.env.URL_NEWS, {
-    next: { revalidate: 3600 },
-  });
-  if (!res.ok) throw new Error("Failed to fetch news");
-  return res.json();
+  const res = await fetch(`${BASE_URL}/api/news`, { next: { revalidate: 3600 } });
+  const text = await res.text();
+
+  try {
+    return JSON.parse(text);
+  } catch (err) {
+    console.error("API response is not valid JSON:", text);
+    return { news: [] }; // fallback
+  }
 }
 
 export default async function NewsPage() {
