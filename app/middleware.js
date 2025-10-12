add746212fa083651749783690a4879e7b050a2d
Amirali Katebi <108225685+amiralikatebi@users.noreply.github.com>
2025-10-12 08:06:03 +0330

    Delete app/middleware.js

--- app/middleware.js
+++
@@ -1,7 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware();
-
-export const config = {
-  matcher: ["/api/:path*"], // فقط مسیرهای API توسط Clerk بررسی میشن
-};
